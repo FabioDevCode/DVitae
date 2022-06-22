f2e241d8705396a4147aa9da2ba9f46b96c66900
FabioDevCode <0677384849@orange.fr>
2022-06-22 22:52:42 +0200

    Fifth Commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: '/DynamicVitae/',
+  base: '/DynamicVitae/',
   transpileDependencies: [
     'vuetify'
   ]
